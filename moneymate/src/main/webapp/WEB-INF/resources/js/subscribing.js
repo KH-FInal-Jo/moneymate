a8a18104a97d30b7ebc3ce3b8206d620ac618f5a
권호진 <khj981008@gmail.com>
2023-08-30 15:46:26 +0900

    구독 진행 중 완료, 구독완료 페이지 진행 중

--- moneymate/src/main/webapp/WEB-INF/resources/js/subscribing.js
+++ moneymate/src/main/webapp/WEB-INF/resources/js/subscribing.js
@@ -18,39 +18,103 @@ const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 
 let temp = haveMile.value;
+let realTemp = realPrice.value;
 allMile.addEventListener("change", () => { /* 전액 사용 */
 
     if(allMile.checked){
         useMile1.value = temp;
         haveMile.value = 0;
-        realPrice.value = parseInt(prePrice) - parseInt(useMile2); // NAN.........ㅋㅋㅋㅋㅋ
+        realPrice.value = parseInt(prePrice.value) - parseInt(useMile1.value);
     } else {
         haveMile.value = temp;
         useMile1.value = 0;
+        realPrice.value = realTemp; /// 수정해야겠다ㅣ.
     }
 
     useMile2.value = useMile1.value;
 
-})
+}) 
 
 useMile1.addEventListener("input", () => { /* 사용 마일리지 누르면 자동으로 밑 금액도 바뀌게 */
+    
     useMile2.value = useMile1.value;
+    
+    
+    if(isNaN(useMile1.value)){ /* 사용 마일리지에 직접 입력하는 경우 */
+        alert("숫자만 입력가능합니다.");
+        useMile2.value=0;
+        useMile1.focus();
+        useMile1.value="";
+    } else {
+        if(parseInt(useMile1.value) > temp){
+            useMile1.value = temp;
+            useMile2.value = temp;
+        }
+    
+        if(useMile1.value == ""){
+            realPrice.value = realTemp;
+            useMile2.value = 0;
+            haveMile.value = temp;
+        } else {
+            realPrice.value = parseInt(prePrice.value) - parseInt(useMile1.value);
+            haveMile.value = temp - parseInt(useMile1.value);
+        }
+    
+        
+
+    }
+
+
+
 })
 
 useMile1.addEventListener("change", () => {
     if(useMile1.value == ""){
-        useMile1.value = 0;
         useMile2.value = 0;
     } 
 })
 
-useMile1.addEventListener("input", () => { /* 사용 마일리지에 직접 입력하는 경우 */
-    if(isNaN(useMile1.value)){
-        alert("숫자만 입력가능합니다.");
-        useMile1.value=0;
-        useMile1.focus();
+useMile1.addEventListener("click", () => {
+    if(useMile1.value == 0){
+        useMile1.value = "";
     }
 })
+
+/* ------------------------------------------------------ */
+
+let flag = true;
+
+/* 무통장입금 버튼 눌렸을 때 */
+cashBtn.addEventListener("click", () => {
+
+    flag = false;
+
+    document.getElementsByClassName("none-area")[0].style.display = "block";
+    document.getElementsByClassName("none-area")[1].style.display = "block";
+    document.getElementsByClassName("none-area")[2].style.display = "block";
+
+    document.getElementById("inform-area").style.display = "none";
+})
+
+creditBtn.addEventListener("click", () => {
+
+    flag = true;
+
+    document.getElementsByClassName("none-area")[0].style.display = "none";
+    document.getElementsByClassName("none-area")[1].style.display = "none";
+    document.getElementsByClassName("none-area")[2].style.display = "none";
+
+    document.getElementById("inform-area").style.display = "block";
+})
+
+
+
+
+
+
+
+
+
 
 Frm.addEventListener("submit", e => {
 
