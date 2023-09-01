0d99c92d738b6a5d1e379d624f17f6e6e34d35df
권호진 <khj981008@gmail.com>
2023-09-01 11:43:14 +0900

    작업 중 가계부 관리 페이지

--- moneymate/src/main/webapp/WEB-INF/resources/js/accountManage.js
+++ moneymate/src/main/webapp/WEB-INF/resources/js/accountManage.js
@@ -3,3 +3,11 @@ $(".hover").mouseleave(
       $(this).removeClass("hover");
     }
   );
+
+
+$("#new").click(function(){
+
+  $("#modal").css("display", "block");
+  $("#main").sytle("backgroundColor", "rgba(0, 0, 0, 0.5)");
+
+})
