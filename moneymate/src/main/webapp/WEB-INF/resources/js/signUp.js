71a67add80d31650e0c0cee60e986672b6323880
banseok <rjh658@naver.com>
2023-09-05 17:10:27 +0900

    11111

--- moneymate/src/main/webapp/WEB-INF/resources/js/signUp.js
+++ moneymate/src/main/webapp/WEB-INF/resources/js/signUp.js
@@ -78,7 +78,7 @@ memberEmail.addEventListener("input", () => {
 // 비밀번호/비밀번호 확인 유효성 검사
 const memberPw = document.getElementById("memberPw");
 const memberPwConfirm = document.getElementById("memberPwConfirm");
-const pwMessage = document.getElementById("pwMessages");
+const pwMessage = document.getElementById("pwMessage");
 
 memberPw.addEventListener("input", ()=> {
 
@@ -97,6 +97,156 @@ memberPw.addEventListener("input", ()=> {
     if(regEx.test(memberPw.value)){
         checkObj.memberPw = true;
 
+        if(memberPwConfirm.value.trim().length == 0){
+            pwMessage.innerText="유효한 비밀번호 형식입니다.";
+            pwMessage.classList.remove("error");
+            pwMessage.classList.add("confirm");
+        } else {
+            if(memberPw.value == memberPwConfirm.value){
+    
+                pwMessage.innerText = "비밀번호가 일치합니다.";
+                pwMessage.classList.remove("error");
+                pwMessage.classList.add("confirm");
+    
+                checkObj.memberPwConfirm=true;
+    
+            } else { 
+    
+            pwMessage.innerText="비밀번호가 일치하지 않습니다.";
+            pwMessage.classList.remove("confirm");
+            pwMessage.classList.add("error");
+    
+            checkObj.memberPwConfirm=false;
+    
+            }
+        }
         
+    } else{
+        pwMessage.innerText="비밀번호 형식이 유효하지 않습니다.";
+        pwMessage.classList.remove("confirm");
+        pwMessage.classList.add("error");
+
+        checkObj.memberPw=false;
     }
-})
+});
+
+// 비밀번호 확인 유효성 검사
+memberPwConfirm.addEventListener("input", () => {
+
+    if(checkObj.memberPw){
+
+        if(memberPw.value == memberPwConfirm.value){
+
+            pwMessage.innerText = "비밀번호가 일치합니다.";
+            pwMessage.classList.remove("error");
+            pwMessage.classList.add("confirm");
+
+            checkObj.memberPwConfirm=true;
+
+        } else { 
+
+        pwMessage.innerText="비밀번호가 일치하지 않습니다.";
+        pwMessage.classList.remove("confirm");
+        pwMessage.classList.add("error");
+
+        checkObj.memberPwConfirm=false;
+
+        }
+    } 
+});
+
+
+// 닉네임 유효성 검사
+const memberNickname = document.getElementById("memberNickname");
+const nickMessage = document.getElementById("nickMessage");
+
+memberNickname.addEventListener("input", ()=> {
+    if(memberNickname.value.trim().length == 0){
+        nickMessage.innerText = "한글,영어,숫자로만 2~10글자"
+        nickMessage.classList.remove("error", "confirm");
+
+        checkObj.memberNickname = false;
+        return;
+    }
+
+    const regEx = /[가-힣\w\d]{2,10}$/;
+
+    if(regEx.test(memberNickname.value)){
+        // 닉네임 중복검사 진행해야함
+        console.log("1");
+
+
+    } else{
+        nickMessage.innerText="유효하지 않은 닉네임 형식입니다.";
+        nickMessage.classList.add("error");
+        nickMessage.classList.remove("confirm");
+
+        checkObj.memberNickname = false;
+    }
+});
+
+
+// 전화번호 유효성 검사
+const memberTel = document.getElementById("memberTel");
+const telMessage = document.getElementById("telMessage");
+
+memberTel.addEventListener("input", () => {
+
+    if(memberTel.value.trim().length == 0){
+        telMessage.innerText = "전화번호를 입력해주세요.(- 제외)";
+        telMessage.classList.remove("error", "confirm");
+
+        checkObj.memberTel = false;
+        return;
+    }
+
+    // 전화번호가 입력되었을 때
+    // 정규표현식s
+    const regEx = /^0(1[01679]|2|[3-6][1-5]|70)\d{3,4}\d{4}$/;
+    // 내가 작성한 정규식 : /^0([1|3|5][\d]|2)\d{7,8}$/
+
+    if(regEx.test(memberTel.value)){
+        telMessage.innerText = "전화번호 형식이 유효합니다.";
+        telMessage.classList.remove("error");
+        telMessage.classList.add("confirm");
+
+        checkObj.memberTel = true;
+    } else {
+        telMessage.innerText = "전화번호 형식이 유효하지 않습니다.";
+        telMessage.classList.remove("confirm");
+        telMessage.classList.add("error");
+
+        checkObj.memberTel = false;
+    }
+});
+
+
+document.getElementById("signUpFrm").addEventListener("submit", e => {
+
+
+    for(let key in checkObj){
+        if(!checkObj[key]){ 
+
+            switch(key){
+                case "check"      : alert("위 약관을 동의해주세요."); break;
+                case "memberEmail"      : alert("이메일이 유효하지 않습니다."); break;
+                case "memberPw"         : alert("비밀번호가 유효하지 않습니다."); break;
+                case "memberPwConfirm"  : alert("비밀번호가 확인되지 않았습니다."); break;
+                case "memberNickname"   : alert("닉네임이 유효하지 않습니다."); break;
+                case "memberTel"        : alert("전화번호가 유효하지 않습니다."); break;
+                case "authKey"        : alert("인증번호가 유효하지 않습니다."); break;
+            }
+
+            document.getElementById(key).focus();
+
+            e.preventDefault();
+            return; 
+        }
+    }
+
+});
+
+
+
+
+
