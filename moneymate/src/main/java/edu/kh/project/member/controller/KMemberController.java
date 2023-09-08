43e3b9f7fac7d303b231759d288ad524b483fcff
sinwook <anzkanzk88@gamil.com>
2023-09-08 15:22:10 +0900

    호진아 힘내! 짜증내도 괜찮아~ ㅋㅋ

--- moneymate/src/main/java/edu/kh/project/member/controller/KMemberController.java
+++ moneymate/src/main/java/edu/kh/project/member/controller/KMemberController.java
@@ -3,23 +3,52 @@ package edu.kh.project.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import edu.kh.project.member.model.dto.Member;
+import edu.kh.project.member.model.service.KMemberService;
 
 @SessionAttributes("loginMember")
 @Controller
 @RequestMapping("/member")
 public class KMemberController {
 	
+	private KMemberService service;
+	
 	@PostMapping("/KmyInfo")
-	public String myInfo() {
+	public String myInfo(Member updateMember, String[]memberAddress
+							, @SessionAttribute("loginMember")Member loginMember
+							, RedirectAttributes ra) {
 		
-		String addr = String.join("^^^", memberAddress);
-		updateMember.setMemberAddress(addr);
+		// 주소 하나로 합치기 (a^^^b^^^c^^^)
+				String addr = String.join("^^^", memberAddress);
+				updateMember.setMemberAddress(addr);
+				
+				// 로그인한 회원의 번호를 updateMember에 추가
+				updateMember.setMemberNo(loginMember.getMemberNo());
+				
+				// DB에 회원정보 수정(UPDATE) 서비스 호출
+				int result = service.updateInfo(updateMember);
+				
+				String message = null;
+				if(result > 0) {
+					// 성공
+					message = "회원 정보가 수정되었습니다.";
+					
+					// Session 에 로그인된 회원 정보도 수정(동기화)
+					loginMember.setMemberNickname(updateMember.getMemberNickname());
+					loginMember.setMemberTel(updateMember.getMemberTel());
+					loginMember.setMemberAddress(updateMember.getMemberAddress());
+				}else {
+					// 실패
+					message = "회원 정보 수정 실패..";
+				}
+				
+				ra.addFlashAttribute("message",message);
 		
-		updateMember.setMemberNo(loginMember.getMemberNo());
-		
-		
-		return "member/KmyInfo";
+		return "redirect:KmyInfo";
 	}
 
 }
--- moneymate/src/main/java/edu/kh/project/member/model/service/KMemberServiceImpl.java
+++ moneymate/src/main/java/edu/kh/project/member/model/service/KMemberServiceImpl.java
@@ -1,8 +1,26 @@
 package edu.kh.project.member.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.kh.project.member.model.dao.KMemberDAO;
+import edu.kh.project.member.model.dto.Member;
 
 @Service
-public class KMemberServiceImpl {
+public class KMemberServiceImpl implements KMemberService{
+	
+	@Autowired
+	private KMemberDAO dao;
+	
+	private BCryptPasswordEncoder bcrypt;
+
+	@Transactional(rollbackFor = {Exception.class})
+	@Override
+	public int updateInfo(Member updateMember) {
+		
+		return dao.updateInfo(updateMember);
+	}
 
 }
