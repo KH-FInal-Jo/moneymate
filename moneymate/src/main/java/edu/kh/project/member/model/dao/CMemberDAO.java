4926ddda2edd14ab10e880da05a6099ce226dba2
banseok <rjh658@naver.com>
2023-09-08 13:51:58 +0900

    1111

--- moneymate/src/main/java/edu/kh/project/member/model/dao/CMemberDAO.java
+++ moneymate/src/main/java/edu/kh/project/member/model/dao/CMemberDAO.java
@@ -1,5 +1,20 @@
 package edu.kh.project.member.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import edu.kh.project.member.model.dto.Member;
+
+@Repository
 public class CMemberDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	public Member login(Member inputMember) {
+		
+		return sqlSession.selectOne("CmemberMapper.login", inputMember);
+	}
 
 }
