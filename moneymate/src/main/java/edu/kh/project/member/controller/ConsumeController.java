637221878e1c43fa17e96f8e40fb9b50dc537931
banseok <rjh658@naver.com>
2023-09-08 17:47:41 +0900

    1111

---
+++ moneymate/src/main/java/edu/kh/project/member/controller/ConsumeController.java
@@ -0,0 +1,17 @@
+package edu.kh.project.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes({"loginMember"})
+public class ConsumeController {
+	
+	
+	
+	
+	
+	
+	
+	
+}
