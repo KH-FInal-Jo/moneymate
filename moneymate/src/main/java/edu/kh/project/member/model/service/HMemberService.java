95fe3e04d4e07b4019aa50dfd5467ad08fa11b7e
hj1008 <khj981008@gmail.com>
2023-09-12 22:26:19 +0900

    좋아요 목록 진행 중

--- moneymate/src/main/java/edu/kh/project/member/model/service/HMemberService.java
+++ moneymate/src/main/java/edu/kh/project/member/model/service/HMemberService.java
@@ -2,6 +2,8 @@ package edu.kh.project.member.model.service;
 
 import java.util.Map;
 
+import edu.kh.project.board.model.dto.CBoard;
+
 public interface HMemberService {
 
 	/** 좋아요 글 목록 조회
@@ -9,5 +11,11 @@ public interface HMemberService {
 	 * @return resMap
 	 */
 	Map<String, Object> selectLike(Map<String, Object> map);
+
+	/** 좋아요 삭제
+	 * @param board
+	 * @return result
+	 */
+	int cancelLike(CBoard board);
 
 }
