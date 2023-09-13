7869659559685e3a478e01db60f12750faae4b3c
sinwook <anzkanzk88@gamil.com>
2023-09-13 14:03:06 +0900

    2시 3분

--- moneymate/src/main/java/edu/kh/project/board/controller/KBoardController.java
+++ moneymate/src/main/java/edu/kh/project/board/controller/KBoardController.java
@@ -2,15 +2,19 @@ package edu.kh.project.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.kh.project.board.model.service.KBoardService;
 
-@RequestMapping("/")
+@RequestMapping("/community")
 @Controller
 public class KBoardController {
 	
 	@Autowired
 	private KBoardService service;
+	
+	public 
+	
 
 }
