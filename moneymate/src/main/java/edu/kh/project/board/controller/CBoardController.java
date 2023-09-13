bfad35cb7c5c6b9152e904b60450f8275f72979a
banseok <rjh658@naver.com>
2023-09-13 16:40:59 +0900

    ㄹㄴㅇㅇㄴㄹ

--- moneymate/src/main/java/edu/kh/project/board/controller/CBoardController.java
+++ moneymate/src/main/java/edu/kh/project/board/controller/CBoardController.java
@@ -1,21 +1,26 @@
 package edu.kh.project.board.controller;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.board.model.dto.CBoard;
@@ -83,6 +88,44 @@ public class CBoardController {
 		model.addAttribute("board",board);
 		
 		return "board/CboardDetail";
+	}
+	
+	// 글쓰기 페이지로 이동
+	@GetMapping("/3/insert")
+	public String boardWrite() {
+		return "board/CBoardWrite";
+	}
+	
+	// 글쓰기
+	@PostMapping("/3/insert")
+	public String boardWrite(CBoard board, @RequestParam(value = "images", required = false) List<MultipartFile> images
+			, @SessionAttribute("loginMember") Member loginMember, RedirectAttributes ra, HttpSession session ) throws IllegalStateException, IOException {
+		
+		
+		board.setMemberNo(loginMember.getMemberNo());
+		board.setBoardCode(3);
+		
+		String webPath = "/resources/images/board/";
+		String filePath = session.getServletContext().getRealPath(webPath);
+		
+		int boardNo = service.boardInsert(board, images, webPath, filePath);
+		
+		String message = null;
+		String path = "redirect:";
+		
+		if(boardNo > 0) { // 성공 시
+			
+			message = "게시글이 등록되었습니다.";
+			path += "/board/3/" + boardNo;
+			
+		} else { // 실패 시
+			message = "게시글 등록 실패";
+			path += "insert";
+		}
+		
+		ra.addFlashAttribute("message", message);
+		
+		return path;
 	}
 	
 	
