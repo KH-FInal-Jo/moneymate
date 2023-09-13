023900a516eb515fa7739dd73bbd89e9cb7e8856
sinwook <anzkanzk88@gamil.com>
2023-09-13 16:41:14 +0900

    다시 푸시

--- moneymate/src/main/java/edu/kh/project/board/model/dao/KBoardDAO.java
+++ moneymate/src/main/java/edu/kh/project/board/model/dao/KBoardDAO.java
@@ -1,8 +1,15 @@
 package edu.kh.project.board.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import edu.kh.project.board.model.dto.CBoard;
+import edu.kh.project.board.model.dto.CPagination;
+import edu.kh.project.board.model.dto.KBoard;
 
 @Repository
 public class KBoardDAO {
@@ -10,9 +17,15 @@ public class KBoardDAO {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 
-	public int getListCount(int boardCode) {
-		
-		return sqlSession.selectOne();
+	public Map<String, Object> getListCount(int boardCode) {
+		return sqlSession.selectOne("KboardMapper.getListCount", boardCode);
 	}
+
+	public List<KBoard> selectBoardList(CPagination pagination, int boardCode) {
+		return null;
+	}
+
+
+	
 
 }
